c56496f16cea3bf965f031007df903a13a74d657
Essence Roberts <essencenroberts@gmail.com>
2026-09-14 14:01:04 -0500

    inital commit, deleted started code, installed React + typescript + tailwindcss, started building custom useFetch hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,60 @@
+import { useEffect } from "react";
+
+
+// create hook useFetch - needs to receive a URL and fetch data 
+function useFetch<T>(url: string){
+
+  // create state to store data
+  const [data, setData] = useState<T | null>(null);
+
+  // create state of API loading 
+  const [loading, setLoading] = useState(true);
+
+  // create state to store error if API request fails
+  const [error, setError] = useState<string | null>(null);
+
+  // useEffect
+  useEffect(() => {
+
+    // function to perform APU request
+    const fetchData = async () => {
+      // load and clear previous errors
+      setLoading(true);
+      setError(null);
+
+      try {
+        // ask the api for data
+        const respone = await fetch(url);
+
+        if(!Response.ok) {
+          throw new Error("Somthine went wrong while fetching data.")
+        }
+
+        //convert API response from JSON into JavaScript
+        const result = await Response.json();
+
+        //put API data into our data state
+        setData(result);
+      } catch (err) {
+        // save an error message if soemthing goes wrong
+        setError(
+          err instanceof Error
+            ? err.message
+            : "Something went wrong."
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return {
+    data,
+    loading,
+    error,
+  };
+}
+
+export default useFetch;
